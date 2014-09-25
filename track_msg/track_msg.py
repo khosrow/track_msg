016b3a554fbc3d52c5b8db1c31102329bbd56908
Khosrow E <khosrow@khosrow.ca>
2014-09-24 22:11:04 -0400

    fix for issue #2

--- track_msg/track_msg.py
+++ track_msg/track_msg.py
@@ -24,18 +24,6 @@ class Message:
         self.color = color
         self.msg_id = msg_id
 
-    def addqid(self, qid):
-        self.qid_list.append(qid)
-
-    def removeqid(self, qid):
-        self.qid_list.remove(qid)
-
-    def hasqid(self, qid):
-        for q in self.qid_list:
-            if qid == q:
-                return True
-        return False
-
 
 def log(msg):
     if DEBUG:
@@ -49,27 +37,69 @@ def print_line(s1, queue_id, s2, color):
         print "%s %s: %s" % (s1, queue_id, s2)
 
 
+def tokenize(line):
+    """tokenize the input line. Returns a tuple if process is postfix.
+    otherwise it returns None
+    """
+    tokens = line.split(None, 6)
+
+    date_stamp = " ".join(tokens[0:3])
+    p = tokens[4].split("/")
+    proc = p[0]
+
+    if proc == "postfix":
+        q = p[1].split("[")
+        daemon = q[0]
+        pid = q[1][:-2]
+        qid = tokens[5][:-1]
+        rol = tokens[6]
+        hol = " ".join(tokens[0:5])
+    else:
+        daemon = None
+        pid = None
+        qid = None
+        rol = None
+        hol = None
+
+    return (date_stamp, proc, daemon, pid, qid, hol, rol)
+
+
 def main():
     global DEBUG
     global COLOR
     msg_list = list()
-    colors = ['red', 'green', 'blue', 'magenta', 'grey', 'cyan', 'white', 'yellow']
+    colors = ['red', 'green', 'blue', 'magenta',
+              'grey', 'cyan', 'white', 'yellow']
     color_counter = 0
 
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description=__doc__, usage="%(prog)s [options] [file [file ...]]")
+    parser = argparse.ArgumentParser(description=__doc__,
+                                     usage="%(prog)s [options] [file [file ...]]")
 
     # One of the 3 options below is needed
-    parser.add_argument("-s", "--sender", help="Email address of the sender")
-    parser.add_argument("-t", "--to", metavar="RECIPIENT", help="Email address of the recipient")
-    parser.add_argument("-m", "--msgid", metavar="MESSAGE-ID", help="Message ID of the email")
+    parser.add_argument("-s", "--sender",
+                        help="Email address of the sender")
+    parser.add_argument("-t", "--to",
+                        metavar="RECIPIENT",
+                        help="Email address of the recipient")
+    parser.add_argument("-m", "--msgid",
+                        metavar="MESSAGE-ID",
+                        help="Message ID of the email")
     # Optional
-    parser.add_argument("-c", "--color", help="Enable colored output", action="store_true")
-    parser.add_argument("-d", "--date", help="Date stamp of the email. Format: <MMM DD>, <MMM DD HH:mm> or <MMM DD HH:mm:ss>")
-    parser.add_argument("-v", "--verbose", help="Display debugging information", action="store_true")
-    parser.add_argument("-V", "--version", action="version", version="%(prog)s {0}".format(appinfo.__version__))
+    parser.add_argument("-c", "--color",
+                        help="Enable colored output",
+                        action="store_true")
+    parser.add_argument("-d", "--date",
+                        help="Date stamp of the email. Format: <MMM DD>, <MMM DD HH:mm> or <MMM DD HH:mm:ss>")
+    parser.add_argument("-v", "--verbose",
+                        help="Display debugging information", action="store_true")
+    parser.add_argument("-V", "--version",
+                        action="version",
+                        version="%(prog)s {0}".format(appinfo.__version__))
     # Files
-    parser.add_argument("file", nargs='*', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument("file", nargs='*',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin)
 
     try:
         args = parser.parse_args()
@@ -103,91 +133,92 @@ def main():
 
     for f in files:
         for line in f:
-            # Break up the log line using spaces. This will give us something like this:
+            # Break up the log line using spaces to get:
             # $DATE $HOSTNAME $PROC/$DAEMON[$PID]: $QID: $RESTOFLINE
             # Note that $QID is not always the queue id of the message.
-            tokens = line.split(None, 6)
-
-            date_stamp = " ".join(tokens[0:3])
-            p = tokens[4].split("/")
-            proc = p[0]
+            date_stamp, proc, daemon, pid, qid, hol, rol = tokenize(line)
 
             if proc == "postfix":
-                q = p[1].split("[")
-                daemon = q[0]
-                pid = q[1][:-2]
-                qid = tokens[5][:-1]
-
                 # smtpd daemon handles all incoming network connections
                 if daemon == "smtpd":
                     if args.sender and args.to:
-                        rest_of_line = tokens[6].lower()
+                        rest_of_line = rol.lower()
+
                         sender = "from=<" + args.sender
                         to = "to=<" + args.to
                     # TODO: fix this naive search
                     # try to match the from=< and to=< portion in tokens[6]
-                    if ( args.sender and 
-                        args.to and 
-                        rest_of_line.find(sender.lower()) > -1 
-                        and rest_of_line.find(to.lower()) > -1):
+                    if (args.sender and args.to and
+                        rest_of_line.find(sender.lower()) > -1 and
+                        rest_of_line.find(to.lower()) > -1):
                         log("Found a matching from/to")
                         found = False
                         if qid == "NOQUEUE":
                             log("Creating new Message. PID: %s" % pid)
-                            msg = Message(pid, date_stamp, colors[color_counter % 8])
+                            msg = Message(pid,
+                                          date_stamp,
+                                          colors[color_counter % 8])
                             color_counter += 1
-                            print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), msg.color)
-                            # in this case we need to grab the next log line 
+                            print_line(hol, qid, rol.rstrip(), msg.color)
+                            # in this case we need to grab the next log line
                             # and get the queue id from it
-                            next_line = next(f)
+                            while True:
+                                next_line = next(f)
 
-                            # TODO: make a tokenizer method
-                            tokens = next_line.split(None, 6)
-                            date_stamp = " ".join(tokens[0:3])
-                            p = tokens[4].split("/")
-                            proc = p[0]
+                                date_stamp, proc, daemon, pid, qid, hol, rol = tokenize(next_line)
 
-                            if proc == "postfix":
-                                q = p[1].split("[")
-                                daemon = q[0]
-                                pid = q[1][:-2]
-                                qid = tokens[5][:-1]
-
-                            log("Storing queue ID for message: %s" % qid)
-                            msg.addqid(qid)
-                            print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), msg.color)
-                            msg_list.append(msg)
+                                if qid != "NOQUEUE":
+                                    log("Storing queue ID for message: %s" % qid)
+                                    msg.qid_list.append(qid)
+                                    print_line(hol,
+                                               qid,
+                                               rol.rstrip(),
+                                               msg.color)
+                                    msg_list.append(msg)
+                                    break
                             continue
 
                         elif msg_list:
                             for m in msg_list:
-                                if q in msg_list.qid_list:
+                                if qid in m.qid_list:
                                     log("Existing message")
                                     found = True
-                                    print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), m.color)
+                                    print_line(hol,
+                                               qid,
+                                               rol.rstrip(),
+                                               m.color)
                                     break
 
                         if qid not in ['NOQUEUE', 'connec', 'disconnec'] and not found:
                             log("Creating new Message. PID: %s" % pid)
                             log("Recording Queue ID: %s" % qid)
-                            msg = Message(pid, date_stamp, colors[color_counter % 8])
-                            msg.addqid(qid)
+                            msg = Message(pid,
+                                          date_stamp,
+                                          colors[color_counter % 8])
+                            msg.qid_list.append(qid)
                             msg_list.append(msg)
                             color_counter += 1
-                            print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), msg.color)
+                            print_line(hol,
+                                       qid,
+                                       rol.rstrip(),
+                                       msg.color)
                     # if it's an smtpd line but doesn't have the matching from/to
                     else:
                         if msg_list:
                             for m in msg_list:
-                                if m.hasqid(qid):
-                                    print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), m.color)
+                                if qid in m.qid_list:
+                                    print_line(hol,
+                                               qid,
+                                               rol.rstrip(),
+                                               m.color)
 
-                # cleanup daemon is what queues the message for the incoming subsystem of postfix
+                # cleanup daemon is what queues the message for
+                # the incoming subsystem of postfix.
                 # We obtain the unique message-id here
                 # example: message-id=<12345@mx.example.com>
                 elif daemon == "cleanup":
                     # msgid = tokens[6].split("<")[1].rstrip()[:-1]
-                    s, sep, m = tokens[6].rstrip().partition('=')
+                    s, sep, m = rol.rstrip().partition('=')
                     if m == "<>" or m == "":
                         msgid = ""
                     elif m[0] == "<":
@@ -200,16 +231,22 @@ def main():
                     if msg_list:
                         for m in msg_list:
                             # if the message is already queued, store the message id
-                            if m.hasqid(qid):
+                            if qid in m.qid_list:
                                 log("Found msg-id of previously queued msg: %s" % qid)
                                 m.msg_id = msgid
-                                print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), m.color)
+                                print_line(hol,
+                                           qid,
+                                           rol.rstrip(),
+                                           m.color)
                                 found = True
                             # here the msg-id has been seen before, and the msg isn't queued previously
                             elif m.msg_id and msgid == m.msg_id:
                                 log("Found new qid for previously seen message: %s" % msgid)
-                                m.addqid(qid)
-                                print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), m.color)
+                                m.qid_list.append(qid)
+                                print_line(hol,
+                                           qid,
+                                           rol.rstrip(),
+                                           m.color)
                                 found = True
 
                     # If the msg-id was passed on command line and we haven't seen it before
@@ -217,20 +254,28 @@ def main():
                         if not found:
                             log("Creating new Message. msg-id: %s" % msgid)
                             log("Recording Queue ID: %s" % qid)
-                            msg = Message(pid, date_stamp, colors[color_counter % 8])
-                            msg.addqid(qid)
+                            msg = Message(pid,
+                                          date_stamp,
+                                          colors[color_counter % 8])
+                            msg.qid_list.append(qid)
                             msg_list.append(msg)
                             color_counter += 1
-                            print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), msg.color)
+                            print_line(hol,
+                                       qid,
+                                       rol.rstrip(),
+                                       msg.color)
 
                 # the qmgr daemon also removed the queued message and we will do the same
                 elif daemon == "qmgr":
                     if msg_list:
                         for m in msg_list:
-                            if m.hasqid(qid):
-                                print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), m.color)
+                            if qid in m.qid_list:
+                                print_line(hol,
+                                           qid,
+                                           rol.rstrip(),
+                                           m.color)
                                 # print line,
-                                if tokens[6].rstrip() == "removed":
+                                if rol.rstrip() == "removed":
                                     log("Removing queued msg: %s " % m.msg_id)
                                     m.qid_list.remove(qid)
 
@@ -238,8 +283,11 @@ def main():
                 else:
                     if msg_list:
                         for m in msg_list:
-                            if m.hasqid(qid):
-                                print_line(" ".join(tokens[0:5]), qid, tokens[6].rstrip(), m.color)
+                            if qid in m.qid_list:
+                                print_line(hol,
+                                           qid,
+                                           rol.rstrip(),
+                                           m.color)
 
     if COLOR:
         color = "white"
